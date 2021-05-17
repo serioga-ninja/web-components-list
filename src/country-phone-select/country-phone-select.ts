65619cc8b8858dfc5a32d8008582e038129e0fbc
Serhii Datsii <sdatsiy@morebis.net>
2021-05-17 18:20:22 +0300

    update

--- src/country-phone-select/country-phone-select.ts
+++ src/country-phone-select/country-phone-select.ts
@@ -1,15 +1,59 @@
-import html from './country-phone-select.html';
-
+import './country-phone-select.less';
+import { countriesInfo } from '../countries-info';
 
 customElements.define('country-phone-select', class extends HTMLElement {
   static get observedAttributes() {
     return ['value', 'country-code', 'country-phone-code'];
   }
 
+  get countryCode() {
+    return this.getAttribute('country-code');
+  }
+
+  set countryCode(value: string) {
+    this.setAttribute('country-code', value);
+  }
+
+  get countryPhoneCode() {
+    return this.getAttribute('country-phone-code');
+  }
+
+  set countryPhoneCode(value: string) {
+    this.setAttribute('country-phone-code', value);
+  }
+
+  get invalid() {
+    return (this.getElementsByTagName('phone-number') as any).invalid;
+  }
+
+  countryObserver: MutationObserver;
+  phoneObserver: MutationObserver;
+
   constructor() {
     super();
 
-    this.innerHTML = html;
+    this.countryObserver = new MutationObserver((mutation) => {
+
+    });
+  }
+
+  render() {
+    const { phoneMask } = countriesInfo
+      .find(row => row.ISO2.toLowerCase() === this.countryCode.toLowerCase())
+
+    this.innerHTML = `
+      <countries-select 
+        country-phone-code="${this.countryPhoneCode}"
+        country-code="${this.countryCode}" 
+      ></countries-select>
+      <phone-input
+        pattern="${phoneMask || ''}"
+      ></phone-input>
+    `;
+  }
+
+  connectedCallback() {
+    this.render();
   }
 
 });
